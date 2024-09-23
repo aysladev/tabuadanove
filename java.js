0dfb4917d7a92a3386361f5c875d3fb7492d5c12
Aysla <aysladev@gmail.com>
2024-09-23 14:39:38 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,7 @@
+for (let numero = 0; numero <= 9; numero++) {
+  console.log("Tabuada do " + numero + ":")
+  for (let i = 0; i <= 10; i++) {
+    console.log(numero + " x " + i + " = " + (numero * i));
+  }
+  console.log("\n"); 
+}
